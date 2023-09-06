39ecf108d1e1168c704a54d0314f54a8ffdfb1de
Mikhail <shaltynovm@gmail.com>
2023-09-06 18:40:40 +0530

    Revert "checking"
    This reverts commit 57b9f541828a1f708c0512a4d1a20e63a0ecdced.

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -3,6 +3,7 @@ package com.example.servlet;
 import com.example.Users;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 
 @WebServlet
@@ -24,9 +26,14 @@ public class LoginServlet extends HttpServlet {
             if (session != null) {
                 String user = Optional.ofNullable((String) session.getAttribute("user")).orElse("");
                 if (user.isEmpty()) {
-                    request.getRequestDispatcher("login.jsp").forward(request, response);
-                } else {
-                    request.getRequestDispatcher("user/hello.jsp").forward(request, response);
+                    ServletContext servletContext = getServletContext();
+                    InputStream resourceStream1 = servletContext.getResourceAsStream("/login.jsp");
+                    InputStream resourceStream2 = servletContext.getResourceAsStream("/user/hello.jsp");
+                    if (resourceStream1 != null && resourceStream2 != null) {
+                        request.getRequestDispatcher("/login.jsp").forward(request, response);
+                    } else {
+                        request.getRequestDispatcher("/user/hello.jsp").forward(request, response);
+                    }
                 }
             }
         }
@@ -34,27 +41,32 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request!=null&&response!=null) {
-            request.getRequestDispatcher("login.jsp");
+        if (request != null && response != null) {
+            request.getRequestDispatcher("/login.jsp");
             String name = request.getParameter("login");
             String password = request.getParameter("password");
             RequestDispatcher requestDispatcher;
-            requestDispatcher = request.getRequestDispatcher("login.jsp");
-            if (name != null && password != null && password.length() > 0) {
-                Users users = Users.getInstance();
-                Optional<String> stringOptional = users.getUsers().stream().filter(s -> s.equals(name)).findAny();
-                HttpSession session = request.getSession(false);
-                if (session != null) {
-                    session.setAttribute("user", name);
-                    if (stringOptional.isPresent()) {
+            ServletContext servletContext = getServletContext();
+            InputStream resourceStream1 = servletContext.getResourceAsStream("/login.jsp");
+            InputStream resourceStream2 = servletContext.getResourceAsStream("/user/hello.jsp");
+            if (resourceStream1 != null && resourceStream2 != null) {
+                requestDispatcher = request.getRequestDispatcher("/login.jsp");
+                if (name != null && password != null && password.length() > 0) {
+                    Users users = Users.getInstance();
+                    Optional<String> stringOptional = users.getUsers().stream().filter(s -> s.equals(name)).findAny();
+                    HttpSession session = request.getSession(false);
+                    if (session != null) {
+                        session.setAttribute("user", name);
+                        if (stringOptional.isPresent()) {
 //                response.sendRedirect(request.getContextPath() + "/user/hello");
-                        requestDispatcher = request.getRequestDispatcher("user/hello.jsp");
+                            requestDispatcher = request.getRequestDispatcher("/user/hello.jsp");
+                        }
                     }
                 }
-            }
 //        response.sendRedirect(request.getContextPath() +"/user/hello.jsp");
-            requestDispatcher.forward(request, response);
+                requestDispatcher.forward(request, response);
 
+            }
         }
     }
 }
